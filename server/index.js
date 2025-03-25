20dc3d80fd8984eb9c646236d7a7c41e1ed1d038
Eshan Singh Parihar <eshaansinghparihar@Eshans-MacBook-Air.local>
2025-03-25 16:50:40 +0530

    Implementation completed

---
+++ server/index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import { configDotenv } from 'dotenv';
+import { Server } from 'socket.io';
+import generateResponse from './prompts/generateResponse.js';
+import getOpenAIResponse from './utils/getOpenAIResponse.js';
+
+configDotenv();
+
+const app = express();
+
+const port = 3001;
+const ioPort = 5000;
+
+const io = new Server(ioPort, {
+    cors: {
+        origin: '*',
+    }
+});
+
+const conversation = [];
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('User disconnected');
+    });
+
+    socket.on('user message', async function (msg) {
+        conversation.push({ role: 'user', content: msg });
+        const prompt = generateResponse(conversation);
+        try {
+            const respone = await getOpenAIResponse(prompt);
+            conversation.push({ role: 'bot', content: respone.data.choices[0].message.content });
+            socket.emit('bot message', respone.data.choices[0].message.content);
+        }
+        catch (error) {
+            console.error('Error while querying OpenAI:', new Error(error));
+            socket.emit('bot message', new Error(error));
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+}
+);
+
---
+++ server/prompts/generateResponse.js
@@ -0,0 +1,7 @@
+export default function generateResponse(conversation)
+{
+    
+    const prompt = `These are the previous chats until now between the user and bot:\n\n ${conversation.map((chat) => `${chat.role}: ${chat.content}`).join('\n')}\n\n As a conversational AI respond to the last message in the above conversations like a human and keep the conversation flowing. Allow for real world context and be empathetic. Search for the right balance between being helpful and being human. You may search the web for infomation and recent news to help you with the conversation.You can be humourous in your responses, but only if necessary.Also there is no need to start the reply with the keyword bot or role : bot.Make sure that the response is relevant to the conversation and is not out of context. Also make sure that this response would be further synthesis to voice, so please avoid using any special characters until necessary.Keep it short and simple.`;
+
+    return prompt
+}
